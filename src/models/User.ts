60c66e5d046d7bac83dd240e51480705d251018a
Kevin Bernfeld <kcbernfeld@gmail.com>
2022-07-20 16:35:41 -0400

    initial commit

---
+++ src/models/User.ts
@@ -0,0 +1,17 @@
+import { Entity, PrimaryGeneratedColumn, Column, BaseEntity } from 'typeorm';
+
+@Entity()
+export class User extends BaseEntity {
+
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  firstName: string;
+
+  @Column()
+  lastName: string;
+
+  @Column()
+  age: number;
+}
